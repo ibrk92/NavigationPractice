d20a6c71654d46e63ad42f8bf0b0558fe597ecff
ibrk92 <ibrahimkiceci@gmail.com>
2023-04-14 14:50:23 -0700

    Using of Fragments, Navigations. Also suing of arguments

--- app/src/main/java/com/ibrahimkiceci/navigationpractice/FirstFragment.java
+++ app/src/main/java/com/ibrahimkiceci/navigationpractice/FirstFragment.java
@@ -2,11 +2,16 @@ package com.ibrahimkiceci.navigationpractice;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 public class FirstFragment extends Fragment {
@@ -30,7 +35,30 @@ public class FirstFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_first, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //Simdi burada setOnClickListener yazdik cunku fragmentlerde ve navigationlarda, goToSecond olarak olusturdugumuz sinifi gormeyebiliyor.
+        // Bunu engellemek icin setOnClickListener diye gorunumlerimizi tanimlayabiliecegimiz OnViewCreated icerisine yazdik.
+        // Istersen hepsini setOnClickListener icerisine yaz ya da goToSecond fonksyonunu senOncLickListener icerisinde cagir nasil istersen
+        // SecondFragment'de de aynisi yapildi.
+
+        Button button = view.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToSecond(view);
+            }
+        });
+    }
+
     public void goToSecond(View view){
+
+        //NavDirections action = FirstFragmentDirections.actionFirstFragmentToSecondFragment(); // Veri gondermeyeceksen bu sekilde yazabilirsin.
+        FirstFragmentDirections.ActionFirstFragmentToSecondFragment action = FirstFragmentDirections.actionFirstFragmentToSecondFragment();
+        action.setAge(50);
+        Navigation.findNavController(view).navigate(action);
 
     }
 }
